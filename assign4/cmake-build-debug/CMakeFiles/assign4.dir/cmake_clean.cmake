7159b0e977c6fa0658d7cd9281cea0efd0feb181
Gradey Cullins <gradeycullins@gmail.com>
2018-04-03 12:02:48 -0600

    Deleted testing text file. Added assign4

---
+++ assign4/cmake-build-debug/CMakeFiles/assign4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assign4.dir/prob_1.c.o"
+  "assign4.pdb"
+  "assign4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/assign4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
